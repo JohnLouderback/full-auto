0855eda20c0534a5afcfcd2d1927b51bb3db4b2b
John Louderback <john@powereddynamics.com>
2024-02-27 23:15:52 -0700

    Added basic buildable WinRT library.

---
+++ DownscalerV3.Cpp.WinRT/DownscalerV3.Cpp.WinRT.cpp
@@ -0,0 +1,13 @@
+// DownscalerV3.Cpp.WinRT.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <winrt/Windows.Graphics.Capture.h>
+
+using namespace winrt::Windows::Graphics::Capture;
+
+// TODO: This is an example of a library function
+void fnDownscalerV3CppWinRT()
+{
+}
