5b9c3e585f0110f23add610377dd8ff26ed0b98a
John Louderback <john@powereddynamics.com>
2025-01-29 18:20:32 -0700

    Stubbed out `IUserConfiguredMonitor`.

---
+++ Core/Contracts/Models/IUserConfiguredMonitor.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Contracts.Models;
+
+public interface IUserConfiguredMonitor {
+  /// <summary>
+  ///   Specifies the order of preference for the user's monitor. For example "1" denotes the display
+  ///   is intended as the user's primary display, "2" denotes the display is intended as the user's
+  ///   secondary display, and so on. The doesn't have any effect on the actual display configuration
+  ///   in the system.
+  /// </summary>
+  public uint UserPreferenceOrder { get; set; }
+}
